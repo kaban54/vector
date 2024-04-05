fb65a7b77c7ebc52d4da7c9a07b0bb3cf79c584c
kaban <tikhonov.as@phystech.edu>
2024-04-05 16:28:53 +0300

    type traits

--- src/concept.hpp
+++ src/concept.hpp
@@ -3,10 +3,10 @@
 
 namespace myconcept {
 
-template<bool B, class T = void>
+template <bool B, typename T = void>
 struct enable_if {};
  
-template<class T>
+template <typename T>
 struct enable_if<true, T> {
     using type = T;
 };
@@ -14,6 +14,52 @@ struct enable_if<true, T> {
 template<bool Cond, typename T = void>
 using enable_if_t = typename enable_if<Cond, T>::type;
 
+
+template <typename T, T val>
+struct integral_constant {
+    static constexpr T value = val;
+    using value_type = T;
+    using type = integral_constant<T, val>;
+    constexpr operator value_type() const noexcept { return value; }
+    constexpr value_type operator()() const noexcept { return value; }
+};
+
+using true_type = integral_constant<bool, true>;
+using false_type = integral_constant<bool, false>;
+
+
+template <typename T, typename U>
+struct is_same : std::false_type {};
+ 
+template <typename T>
+struct is_same<T, T> : std::true_type {};
+
+template <typename T, typename U>
+using is_same_v = is_same<T, U>::value;
+
+
+template <typename T>
+struct is_arithmetic : integral_constant<bool, std::is_integral<T>::value ||
+                                               std::is_floating_point<T>::value> {};
+template <typename T>
+using is_arithmetic_v = is_arithmetic::value;
+
+
+// template <typename T>
+// struct is_enum : integral_constant<bool, > {};
+// template <typename T>
+// using is_enum_v = is_enum::value;
+
+
+
+template<typename T>
+struct is_scalar : integral_constant<bool, is_arithmetic<T>::value || 
+                                           std::is_enum<T>::value || 
+                                           std::is_pointer<T>::value ||
+                                           std::is_member_pointer<T>::value || 
+                                           std::is_null_pointer<T>::value> {};
+template <typename T>
+using is_scalar_v = is_scalar::value;
 
 
 template <typename T>
